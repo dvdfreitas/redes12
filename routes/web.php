e38cb60d1f6f6dec0b1f430118e60d81b0715926
David Freitas <dvdfreitas@gmail.com>
2025-02-19 14:02:33 +0000

    Image Upload v1.0

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,50 @@
 <?php
 
 use App\Models\Category;
+use App\Models\Image;
 use App\Models\Race;
 use App\Models\Story;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {    
-    return view('welcome');
-});
+// Route::get('/', function () {    
+//     return view('welcome');
+// });
 
 Route::get('/stories', function () {
     $stories = Story::with('categories')->get();    
     return view('stories.index', compact('stories'));
  });
+
+
+ Route::get('/', function () {    
+    $images = Image::all();
+    return view('/images', compact('images'));
+});
+
+Route::get('/image_upload', function () {    
+    return view('image_upload');
+});
+
+Route::post('/image_upload/store', function (Request $request) {    
+
+    $request->validate([
+        'name' => 'required',
+        'image' => 'required|image|mimes:jpeg,png|max:2048'
+    ]);
+
+    $imageName = time() . "." . $request->image->extension();
+    $request->image->move(public_path('images'), $imageName);
+
+    Image::create([
+        'name' => $request['name'],
+        'path' => $imageName
+    ]);
+
+    return redirect('/');
+});
+
+
 
 Route::get('/races', function () {    
     return view('races.index');
